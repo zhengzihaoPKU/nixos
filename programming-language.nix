acab9bddc5881ce4fcc077fe439714afc24e5cc6
zhengzihaoPKU <zhengzihao@stu.pku.edu.cn>
2026-01-14 11:00:29 +0800

    revise
    	新文件：   gnome.nix
    	修改：     programming-language.nix

--- programming-language.nix
+++ programming-language.nix
@@ -3,8 +3,13 @@
 {
   environment.systemPackages = with pkgs; [
     # go
+<<<<<<< HEAD
     (python313.withPackages(ps: with ps; [ pygobject3 ]))
     uv
+=======
+   # (python313.withPackages(ps: with ps; [ pygobject3 ]))
+   # uv
+>>>>>>> 08db6f7 (revise)
     # nodePackages_latest.nodejs
     # nodePackages_latest.pnpm
     # bun
